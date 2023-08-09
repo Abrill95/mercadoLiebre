b52265444453df31f7c2bae5d5f76be1eaff27bb
Abrill95 <showna9555@gmail.com>
2023-08-09 15:43:12 -0300

    modificacion

--- src/app.js
+++ src/app.js
@@ -3,16 +3,20 @@ const app = express()
 
 const path = require("path");
 
-app.listen(port, ()=> console.log("servidor escuchando en el puerto 3001"))
+app.listen(3001, ()=> console.log("servidor escuchando en el puerto 3000"))
 
 app.use(express.static("public"));
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
-app.get("/contactame", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/contacto.html"));
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"));
 });
