dc01601471cc8dd499b1ed1011c8b0ee41e09889
Abrill95 <showna9555@gmail.com>
2023-08-09 15:59:47 -0300

    probado1

--- src/app.js
+++ src/app.js
@@ -2,21 +2,19 @@ const express= require("express");
 const app = express()
 
 const path = require("path");
-
-app.listen(3001, ()=> console.log("servidor escuchando en el puerto 3000"))
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log("servidor escuchando en el puerto 3000"));
 
 app.use(express.static("public"));
 
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
-app.get("/register", (req, res) => {
+app.get("/register", (_req, res) => {
     res.sendFile(path.join(__dirname, "./views/register.html"));
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", (_req, res) => {
     res.sendFile(path.join(__dirname, "./views/login.html"));
 });
