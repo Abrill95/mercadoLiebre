5a29e9aac5a942615742e00f4c00622c1dae68f0
Abrill95 <showna9555@gmail.com>
2023-08-09 11:30:08 -0300

    Agregando estilos con css

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express= require("express");
+const app = express()
+
+const path = require("path");
+
+app.listen(3001, ()=> console.log("servidor escuchando en el puerto 3001"))
+
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/contactame", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/contacto.html"));
+});
